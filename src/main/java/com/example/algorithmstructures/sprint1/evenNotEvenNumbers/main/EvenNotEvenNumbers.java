492b8e7cbc8ce710f31272764a377ce4c835a95b
Artem <artym.surkov@mail.ru>
2022-12-09 02:48:10 +0500

    Задача: Четные и не четные числа

---
+++ src/main/java/com/example/algorithmstructures/sprint1/evenNotEvenNumbers/main/EvenNotEvenNumbers.java
@@ -0,0 +1,29 @@
+package com.example.algorithmstructures.sprint1.evenNotEvenNumbers.main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class EvenNotEvenNumbers {
+  public static String isEven(int a, int b, int c) {
+    if ((a % 2 == 0 && b % 2 == 0 && c % 2 == 0) ||
+        (a % 2 != 0 && b % 2 != 0 && c % 2 != 0)) {
+      return "YES";
+    } else {
+      return "NO";
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    StringBuilder output_buffer = new StringBuilder();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+    int a = Integer.parseInt(tokenizer.nextToken());
+    int b = Integer.parseInt(tokenizer.nextToken());
+    int c = Integer.parseInt(tokenizer.nextToken());
+    output_buffer.append(isEven(a, b, c)).append("\n");
+    System.out.println(output_buffer);
+  }
+}
---
+++ src/main/java/com/example/algorithmstructures/sprint1/evenNotEvenNumbers/test/EvenNotEvenNumbersTest.java
@@ -0,0 +1,71 @@
+package com.example.algorithmstructures.sprint1.evenNotEvenNumbers.test;
+
+import static com.example.algorithmstructures.sprint1.evenNotEvenNumbers.main.EvenNotEvenNumbers.isEven;
+import static java.lang.System.out;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.opentest4j.AssertionFailedError;
+
+/**
+ * Тесты для решения задачки "Четное/не четное".
+ * Time out у этой задачки равен - 1(с).
+ * p.s Обратите внимания, что даже если тест получил статус [OK]
+ * вероятна ошиюка TL - time limited expected, что значит -
+ * алгоритм превысил установленный лимит времени.
+ */
+@Timeout(value = 1000, unit = TimeUnit.MILLISECONDS)
+public class EvenNotEvenNumbersTest {
+
+  final private String OK = "Тест %s [OK].";
+  final private String WA = """
+      Тест %s [WA] Wrong Answer.
+      Ответ неверен.
+      Возможная причина: Ошибка в программе или неверный алгоритм.""";
+
+  @Test
+  @DisplayName("Тест 1")
+  public void test_1() {
+    String expected = "NO";
+    String actual = isEven(1, 2, -3);
+    try {
+      Assertions.assertEquals(expected, actual);
+      out.printf((OK) + "%n", 1);
+    } catch (AssertionFailedError assertionFailedError) {
+      out.printf((WA) + "%n", 1);
+      out.println(assertionFailedError.getMessage());
+    }
+  }
+
+  @Test
+  @DisplayName("Тест 2")
+  public void test2() {
+    String expected = "YES";
+    String actual = isEven(7, 11, 7);
+    try {
+      Assertions.assertEquals(expected, actual);
+      out.printf((OK) + "%n", 2);
+    } catch (AssertionFailedError assertionFailedError) {
+      out.printf((WA) + "%n", 2);
+      out.println(assertionFailedError.getMessage());
+    }
+  }
+
+  @Test
+  @DisplayName("Тест 3")
+  public void test3() {
+    String expected = "YES";
+    String actual = isEven(6, -2, 0);
+    try {
+      Assertions.assertEquals(expected, actual);
+      out.printf((OK) + "%n", 3);
+    } catch (AssertionFailedError assertionFailedError) {
+      out.printf((WA) + "%n", 3);
+      out.println(assertionFailedError.getMessage());
+    }
+  }
+
+}
